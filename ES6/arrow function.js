f36ab360f645e48cca068c129b5a107bd361fee0
Quazi Samiha <quazisamiha@gmail.com>
2023-09-05 10:51:05 -0700

    fat arror function

--- ES6/arrow function.js
+++ ES6/arrow function.js
@@ -1,7 +1,34 @@
 // ES6 FAT ARROW FUNCTION
 
-function number() {
-    return 10;
-}
+// normal funciton
+// function number() {
+//     return 10;
+// }
 
-console.log(number())
+// console.log(number())
+
+// arrow function
+// let number = () => {
+//     return 10;
+// }
+
+// console.log(number());
+
+// simplest format of arrow function with one statement that return something
+// let number = () => 10;
+// console.log(number());
+
+// if we use return in single statement it won't work
+// let number = () => return 10;
+// console.log(number());
+
+// let number = () => console.log(10);
+// number();
+
+// if we use one parameter , we can omit braces
+// let number = a => a;
+// console.log(number(25));
+
+// but if we use more than one parameter, we have to use braces
+// let number = (a, b) => a + b;
+// console.log(number(12, 25));
