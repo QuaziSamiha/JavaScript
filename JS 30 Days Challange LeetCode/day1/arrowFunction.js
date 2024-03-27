836219f64a3e422377e71d7814f24f9ab48377cd
QuaziSamiha <1703155@student.ruet.ac.bd>
2024-03-27 23:54:32 +0600

    closure in js

---
+++ JS 30 Days Challange LeetCode/day1/arrowFunction.js
@@ -0,0 +1,13 @@
+// Arrow Syntax
+const f = (a, b) => {
+    const sum = a + b;
+    return sum;
+};
+console.log(f(3, 4)); // 7
+
+/**
+ * In this example, f is the name of the function. 
+ * (a, b) are the arguments. You can write any logic in the body and 
+ * finally return a result. You are allowed to return nothing, and it will instead 
+ * implicitly return undefined.
+ */
