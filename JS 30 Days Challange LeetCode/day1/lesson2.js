5807bbe1236f45d41228934da80b44b29f8cb18e
QuaziSamiha <1703155@student.ruet.ac.bd>
2024-03-27 23:36:51 +0600

    annonymous function 1

---
+++ JS 30 Days Challange LeetCode/day1/lesson2.js
@@ -0,0 +1,10 @@
+var f = function(a, b) {
+    const sum = a + b;
+    return sum;
+}
+console.log(f(3, 4)); // 7
+
+// Anonymous Function
+// You can optionally exclude the name of the function after the function keyword.
+// In JavaScript, an anonymous function is a function that does not have a name. 
+
